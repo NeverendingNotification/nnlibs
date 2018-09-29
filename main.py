88b39df60db9b4e95091664d3dcf1a3805688fff
naoki <nonimarukaku@gmail.com>
2018-09-30 08:37:11 +0900

    basic code

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 07:33:54 2018
+
+@author: nn
+"""
+
+from manager import get_loader, get_trainer, get_runner
+
+def main(settings):
+  loader_setting = settings["loader"]
+  trainer_setting = settings["trainer"]
+  runner_setting = settings["runner"]
+  
+  loader = get_loader(loader_setting)
+  trainer = get_trainer(trainer_setting, loader)
+  runner = get_runner(runner_setting, loader, trainer)
+  
+  runner.run()
+  
+
+  
+  
+  
---
+++ manager.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 08:03:51 2018
+
+@author: nn
+"""
+
+
+
+
