ec9f0e37899f4ca99765266b5e1c89692b2c62d4
naoki <nonimarukaku@gmail.com>
2018-10-07 08:04:00 +0900

    add captions

--- analyzer/html_viewer.py
+++ analyzer/html_viewer.py
@@ -12,13 +12,28 @@ import xml
 import xml.etree.ElementTree as ET
 import numpy as np
 
-def make_html(img_paths, out_file):
+def make_html(img_paths, out_file, captions=[]):
   root = ET.Element("html")
   header = ET.SubElement(root, "header")
   body = ET.SubElement(root, "body")
+
+  n_col = 5
+  table = ET.SubElement(body, "table")
+    
   
-  for img in img_paths:
-    img_tag = ET.SubElement(body, "img", attrib={"src":img})
+  for i, img in enumerate(img_paths):
+    if i % n_col == 0:
+      tr = ET.SubElement(body, "tr")
+    attrib = {"src":img, "width":"128", "height":"128"}
+    th = ET.SubElement(tr, "th")
+    fig = ET.SubElement(th, "figure")
+    img_tag = ET.SubElement(fig, "img", attrib=attrib)
+    cap = ET.SubElement(fig, "figcaption")
+    cap_info = [os.path.basename(img)]
+    if len(captions) == len(img_paths):
+      cap_info.extend(captions[i])
+    cap_text = "\n".join(cap_info)
+    cap.text = os.path.basename(cap_text)
   ET.ElementTree(root).write(out_file)
 
 def make_html_imgs(img_dir, out_file, sample_mode=100, random=True):
