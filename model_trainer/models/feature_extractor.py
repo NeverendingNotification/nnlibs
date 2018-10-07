9355820ca5f095c76a7aa0e2e44ee58655658bf7
naoki <nonimarukaku@gmail.com>
2018-10-08 08:56:07 +0900

    add vae

--- model_trainer/models/feature_extractor.py
+++ model_trainer/models/feature_extractor.py
@@ -14,10 +14,18 @@ def global_average_pooling(conv_output):
   return out
 
 
-def get_feature_extractor(conv_output, feature_type):
-  if feature_type == "gap":
-    return global_average_pooling(conv_output)
-  elif feature_type == "flatten":
-    return tf.layers.flatten(conv_output, name="flatten")
-  else:
-    raise NotImplementedError()
+def get_feature_extractor(conv_output, feature_type,
+                          var_name="feature"):
+  with tf.variable_scope(var_name):
+    if feature_type == "gap":
+      return global_average_pooling(conv_output)
+    elif feature_type == "flatten":
+      return tf.layers.flatten(conv_output, name="flatten")
+    elif feature_type == "vae":
+      gap = global_average_pooling(conv_output)
+      n_feature = int(gap.shape[1])
+      mu = tf.layers.dense(gap, n_feature)
+      sigma = tf.layers.dense(gap, n_feature)
+      return mu + sigma * tf.random_normal(tf.shape(gap), 0, 1)
+    else:
+      raise NotImplementedError()
